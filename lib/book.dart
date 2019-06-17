5d84da139f827873f9aaafe1c39a507ebb37b966
kassemx1 <ksmmariam0103>
2019-06-17 16:20:03 +0300

    Initial commit

---
+++ lib/book.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class book {
+  String name;
+  String imagename;
+  String details;
+  String categorie;
+  book(String name,String imagename,String details,String categories){
+    this.name = name;
+    this.imagename = imagename;
+    this.details =details;
+    this.categorie= categories;
+
+  }
+}
